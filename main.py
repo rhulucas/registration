3ad68ae75841b6130a6af9dcfcd5a0543467964f
Rong Hu <144379767+rhulucas@users.noreply.github.com>
2023-12-17 22:28:17 +0000

    add @app.get about movie review and commentary

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app =FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"Hello": "World"}
+
+@app.get("/movie/{movie_name}")
+def get_movieview_by_name(movie_name: str):
+    results = []
+    if movie_name:
+        movies = Movie(movie_name)
+        movies.youtube_search(max_result=10)
+        return movies._review_videos
+    return results
